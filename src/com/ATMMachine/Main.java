31ceae8b65c73263f147c6ac604ccaeb1336680c
K4voos <55686856+K4voos@users.noreply.github.com>
2021-01-08 00:35:07 +0330

    Classes created and functions through the stdout are working

--- src/com/ATMMachine/Main.java
+++ src/com/ATMMachine/Main.java
@@ -1,8 +1,11 @@
 package com.ATMMachine;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        ATMMenu account = new ATMMenu();
+        account.getLogin();
     }
 }
