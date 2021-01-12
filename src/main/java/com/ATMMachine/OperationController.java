54ab1730dcfaffee72834b7a3cfb93a07184acc2
K4voos <55686856+K4voos@users.noreply.github.com>
2021-01-12 08:36:07 +0330

    Operation page created but not finished

---
+++ src/main/java/com/ATMMachine/OperationController.java
@@ -0,0 +1,36 @@
+package com.ATMMachine;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class OperationController {
+    public Button depoBTN;
+    public Button withdrawBTN;
+    public Button logOutBTN;
+    @FXML
+    public Label alertText;
+    public Button transferBTN;
+    public Button changeAccBTN;
+
+    public void depoOnClick(ActionEvent event) {
+    }
+
+    public void withdrawOnClick(ActionEvent event) {
+    }
+
+    public void logOutOnClick(ActionEvent event) throws IOException {
+        Main.changeScene("/LogIn.fxml");
+    }
+
+    public void transferOnClick(ActionEvent event) {
+        alertText.setText("Unfortunately this option is not available now");
+    }
+
+    public void changeAccOnClick(ActionEvent event) throws IOException {
+        Main.changeScene("/GetAccount.fxml");
+    }
+}
