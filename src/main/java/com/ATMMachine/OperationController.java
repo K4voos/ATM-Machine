5350841a17e66ebc19045761dfe05f23282704be
K4voos <55686856+K4voos@users.noreply.github.com>
2021-01-12 08:43:30 +0330

    View Balance BTN added to operation screen

--- src/main/java/com/ATMMachine/OperationController.java
+++ src/main/java/com/ATMMachine/OperationController.java
@@ -15,6 +15,7 @@ public class OperationController {
     public Label alertText;
     public Button transferBTN;
     public Button changeAccBTN;
+    public Button viewBalanceBTn;
 
     public void depoOnClick(ActionEvent event) {
     }
@@ -32,5 +33,8 @@ public class OperationController {
 
     public void changeAccOnClick(ActionEvent event) throws IOException {
         Main.changeScene("/GetAccount.fxml");
+    }
+
+    public void viewBalanceOnClick(ActionEvent event) {
     }
 }
