fb92ed48a730e3b701d102f0fbe3facc774fe5e0
K4voos <55686856+K4voos@users.noreply.github.com>
2021-01-12 16:27:55 +0330

    zombie code removed

--- src/main/java/com/ATMMachine/Main.java
+++ src/main/java/com/ATMMachine/Main.java
@@ -29,7 +29,5 @@ public class Main extends Application {
 
     public static void main(String[] args) throws IOException {
         launch(args);
-//        ATMMenu account = new ATMMenu();
-//        account.getLogin();
     }
 }
