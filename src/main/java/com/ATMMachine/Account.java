e4601f51ec2791ee6c64826d1b76137b5b1292bb
K4voos <55686856+K4voos@users.noreply.github.com>
2021-01-12 16:11:51 +0330

    extra codes removed or moved to OperationController class

--- src/main/java/com/ATMMachine/Account.java
+++ src/main/java/com/ATMMachine/Account.java
@@ -6,7 +6,7 @@ public class Account {
     private int customerNumber, PIN;
     private double SavingBalance, checkingBalance;
 
-    DecimalFormat format = new DecimalFormat("'$'###,##0.00");
+    DecimalFormat format = new DecimalFormat("'$'###,##0.00"); //todo add Locale.US
 
     public double getCheckingBalance() {
         return checkingBalance;
@@ -38,45 +38,5 @@ public class Account {
 
     public void setPIN(int PIN) {
         this.PIN = PIN;
-    }
-
-    public void savingWithdraw(int amount) {
-        if (amount > getSavingBalance()) {
-            System.out.println("Balance cannot be negative.");
-        } else {
-            setSavingBalance(getSavingBalance() - amount);
-            System.out.println("Remaining balance: " + format.format(getSavingBalance()));
-        }
-    }
-
-    public void savingDeposit(int amount) {
-        if (amount < 0) {
-            System.out.println("You cannot deposit a negative amount.");
-        } else if (amount == 0) {
-            System.out.println("Nothing added.");
-        } else {
-            setSavingBalance(getSavingBalance() + amount);
-            System.out.println("New balance: " + format.format(getSavingBalance()));
-        }
-    }
-
-    public void checkingWithdraw(int amount) {
-        if (amount > getCheckingBalance()) {
-            System.out.println("Balance cannot be negative.");
-        } else {
-            setCheckingBalance(getCheckingBalance() - amount);
-            System.out.println("Remaining balance: " + format.format(getCheckingBalance()));
-        }
-    }
-
-    public void checkingDeposit(int amount) {
-        if (amount < 0) {
-            System.out.println("You cannot deposit a negative amount.");
-        } else if (amount == 0) {
-            System.out.println("Nothing added.");
-        } else {
-            setCheckingBalance(getCheckingBalance() + amount);
-            System.out.println("New balance: " + format.format(getCheckingBalance()));
-        }
     }
 }
